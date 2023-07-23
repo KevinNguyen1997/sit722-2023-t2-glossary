4f7956263c8351589bfe011c6ecbe09230578a0a
KevinNguyen1997 <101107863+KevinNguyen1997@users.noreply.github.com>
2023-07-23 21:36:49 +1000

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
-const { glosarries } = require("../consts");
+const { glosarries } = require("../glossary");
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 
     title: 'SIT722 Software Deployment and Operation',
-    glossaries: glossaries,
+    glossaries: glosarries,
   });
 });
 
 module.exports = router;
+
