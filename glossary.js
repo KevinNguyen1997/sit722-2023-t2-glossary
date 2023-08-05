39c2d73dc6c5ed2fbe959066117321a70fe8cf2a
KevinNguyen1997 <101107863+KevinNguyen1997@users.noreply.github.com>
2023-08-05 23:59:12 +1000

    Update glossary.js

--- glossary.js
+++ glossary.js
@@ -75,5 +75,83 @@ const glosarries = [
       reference:
         "https://cloud.google.com/discover/what-is-container-orchestration#:~:text=Container%20orchestration%20automatically%20provisions%2C%20deploys,life%20cycle%20management%20of%20containers.",
     },
+    {
+    id: 11,
+    term: "Continuous Monitoring",
+    description:
+      "Continuous monitoring refers to the practice of constantly observing and analyzing various aspects of a system, process, or service to ensure its smooth functioning and performance. It involves real-time or regular data collection, analysis, and feedback to identify potential issues, optimize performance, and make informed decisions for ongoing improvements.",
+    reference:
+      "https://www.crowdstrike.com/cybersecurity-101/observability/continuous-monitoring/#:~:text=Continuous%20monitoring%20is%20an%20approach,time%20to%20address%20them%20quickly.",
+  },
+  {
+    id: 12,
+    term: "Behavior-driven design (BDD) ",
+    description:
+      "Behavior-driven design (BDD) is an approach to software development that focuses on clear communication and collaboration between developers, testers, and business stakeholders. It uses natural language statements to describe how the software should behave in different scenarios. Automated tests are then created based on these statements to ensure the software works as expected according to the specified behaviors.",
+    reference: "https://cucumber.io/docs/bdd/",
+  },
+  {
+    id: 13,
+    term: "Customer-centric action",
+    description:
+      "DevOps should be driven by a customer-first approach. All decisions and actions taken by the team should prioritize customer satisfaction and experience. By aligning with customer needs, DevOps can deliver solutions that better meet expectations and address pain points.",
+    reference:
+      "P Mateos,https://www.pufferfish.solutions/post/customer-centric-action-devops-principal-1",
+  },
+  {
+    id: 14,
+    term: "Cross-functional autonomous teams",
+    description:
+      "In companies that build products with self-contained teams, these teams must have the freedom to work independently on the product from start to finish. This includes having all the necessary skills and knowledge to take full responsibility for the entire process.",
+    reference:
+      "P Mateos, https://www.pufferfish.solutions/post/cross-functional-autonomous-teams-devops-principle-4#:~:text=In%20product%20organisations%20with%20vertical,end%2Dto%2Dend%20responsibility.",
+  },
+  {
+    id: 15,
+    term: "End-To-End Responsibility:",
+    description:
+      "The DevOps team improves product quality and takes more responsibility by providing performance support until the products reach their end-of-life stage.",
+    reference: "https://www.devopsagileskills.org/dasa-devops-principles/",
+  },
+  {
+    id: 16,
+    term: "Test-driven design (TDD)",
+    description:
+      "Test-driven design (TDD) is a software development approach where developers write automated tests before writing the actual code. The process involves three main steps: writing a test that describes the desired behavior, running the test (which initially fails), and then writing the code to make the test pass. TDD helps ensure that the code is thoroughly tested, improves code quality, and guides developers to build software that meets the desired requirements.",
+    reference: "https://www.spiceworks.com/tech/devops/articles/what-is-tdd/",
+  },
+  {
+    id: 17,
+    term: "Agile Methodologies",
+    description:
+      "Agile methodologies are flexible and collaborative approaches to building software. They involve working in short cycles, delivering small pieces of the product regularly. By involving customers, adapting to changes, and focusing on teamwork, agile methods help create better software that meets customer needs faster and more efficiently.",
+    reference:
+      "https://www.atlassian.com/agile#:~:text=The%20Agile%20methodology%20is%20a,READ%20ON%20BELOW",
+  },
+  {
+    id: 18,
+    term: "Continuous improvement",
+    description:
+      "Continuous Improvement is at the core of the DevOps culture, aiming to reduce waste and enhance the speed of product or service enhancements. This constant drive for improvement ensures a continuous and iterative refinement of the offerings.",
+    reference:
+      "https://tkssharma-devops.gitbook.io/devops-training/what-is-devops/devops-principles",
+  },
+  {
+    id: 19,
+    term: "Package Management",
+    description:
+      "A package management system, also known as a package manager, comprises a set of tools that facilitate the creation of complete packages ready for release and deployment. It automates tasks like installing, upgrading, downgrading, configuring, removing, and purging software, streamlining the software management process.",
+    reference:
+      "https://www.futurelearn.com/info/courses/cloudswyft-msft-introduction-to-devdps-practices/0/steps/184484#:~:text=about%20package%20management.-,A%20package%20management%20system%20or%20package%20manager%20is%20the%20set,configuring%2C%20removing%20and%20purging%20software.",
+  },
+  {
+    id: 20,
+    term: "CI Server",
+    description:
+      "A continuous integration server, also known as a build server, plays a crucial role in managing the shared repository and overseeing incoming code changes. Whenever developers commit their code to the repository, the CI server automatically triggers a build process and records the outcomes of the build.",
+    reference:
+      "https://about.gitlab.com/topics/ci-cd/continuous-integration-server/#:~:text=What%20does%20a%20CI%20server,the%20results%20of%20the%20build.",
+  },
   ];
+
   module.exports = { glosarries };
